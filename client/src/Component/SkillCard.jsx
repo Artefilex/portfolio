5fc822e461cc67f925164ea87e10701af48ec755
Artefilex <baris.tncdmr@gmail.com>
2023-07-18 17:32:12 +0300

    skillcard modified

--- client/src/Component/SkillCard.jsx
+++ client/src/Component/SkillCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import{ BiLogoJavascript,BiLogoGithub } from "react-icons/bi";
+import { BiLogoJavascript, BiLogoGithub } from "react-icons/bi";
 import { FaReact, FaNodeJs } from "react-icons/fa";
 
 function SkillCard() {
@@ -9,11 +9,31 @@ function SkillCard() {
 
       <div className="skillcard-context flex">
         <p>I'm thrilled to share my 2.5-year learning journey with you! </p>
-        <ul>
-          <li><BiLogoGithub className="icon github"/> 24 repositories on GitHub</li>
-          <li><BiLogoJavascript className="icon javascript"/> 7 HTML CSS JS Projects</li>
-          <li><FaReact className="icon react"/> 8 React Projects</li>
-          <li><FaNodeJs className="icon nodejs"/> 2 Node.js Projects</li>
+        <ul className="flex">
+          <li className="flex">
+            <div className="info-icon">
+              <BiLogoGithub className="icon github" /> <span>24</span>
+            </div>
+            <p> repositories on GitHub</p>
+          </li>
+          <li className="flex">
+            <div className="info-icon">
+              <BiLogoJavascript className="icon javascript" /> <span>7</span>
+            </div>
+            <p> HTML CSS JS Projects</p>
+          </li>
+          <li className="flex">
+            <div className="info-icon">      
+              <FaReact className="icon react" /> <span>8</span>
+            </div>
+            <p>React Projects</p>
+          </li>
+          <li className="flex">
+            <div className="info-icon">
+              <FaNodeJs className="icon nodejs" /> <span>2</span>
+            </div>
+            <p> Node Projects</p>
+          </li>
         </ul>
       </div>
     </div>
